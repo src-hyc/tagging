333d70eadb1ca287145084fe69b8f22e155dfdd1
Hyc <yhou17@simons-rock.edu>
2022-02-23 00:40:47 -0500

    Add README.md Add comments to code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tagging
+
+Tagging is a tagging library with JavaScript and MongoDB.
+
+## Usage
+
+```javascript
+const tagger = new Tagger(collection);
+
+tagger.tagKey("key", "tag");
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- tagger/tagger.js
+++ tagger/tagger.js
@@ -1,12 +1,22 @@
 import { MongoClient } from 'mongodb';
 
+/** Tagger class stores taggings in a MongoDB collection */
 export default class Tagger {
 	#collection;
 
+	/**
+	 * @param {Collection} collection A MongoDB collection
+	 */
 	constructor(collection) {
 		this.#collection = collection;
 	}
 
+	/**
+	 * Tag a key with a tag
+	 * @param {string} key Key to be tagged on
+	 * @param {!Array<string>} tag Tag to be tagged
+	 * @return {!Promise<undefined>} Resolves on success, rejects on failure
+	 */
 	async tagKey(key, tag) {
 		return this.#collection.updateOne(
 			{ "_id": key },
@@ -14,15 +24,23 @@ export default class Tagger {
 				$addToSet: { "tagSet": tag },
 			},
 			{ upsert: true },
-		);
+		)
+		.then(() => {});
 	}
 
+	/**
+	 * Untag a tag from a key
+	 * @param {string} key Key to be untagged from
+	 * @param {!Array<string>} tag Tag to be untagged
+	 * @return {!Promise<undefined>} Resolves on success, rejects on failure
+	 */
 	async untagKey(key, tag) {
 		return this.#collection.updateOne(
 			{ "_id": key},
 			{
 				$pull: { "tagSet": tag },
 			},
-		);
+		)
+		.then(() => {});
 	}
 }
