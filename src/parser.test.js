91e3231cd4ba4ed45b21c7ecfcb753d8abb6d260
Hyc <yhou17@simons-rock.edu>
2022-03-03 21:29:01 -0500

    Add code to parse a string that represents a tag

---
+++ src/parser.test.js
@@ -0,0 +1,13 @@
+import { parseTag } from './parser.js';
+
+test('Parse a tag of 1 level', () => {
+	expect(parseTag("tag")).toEqual([ "tag" ]);
+});
+
+test('Parse a tag of 2 levels', () => {
+	expect(parseTag("tag_parent/tag_child")).toEqual([ "tag_parent", "tag_child" ]);
+});
+
+test('Parse a tag of 3 levels', () => {
+	expect(parseTag("tag_level1/tag_level2/tag_level3")).toEqual([ "tag_level1", "tag_level2", "tag_level3" ]);
+});
